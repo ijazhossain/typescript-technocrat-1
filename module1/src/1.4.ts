7e8332928822753fbd6703757c23718da8e5328c
ijazhossain <ijaz.hossain35@gmail.com>
2024-03-25 12:55:10 +0600

    1.4.ts added

---
+++ module1/src/1.4.ts
@@ -0,0 +1,17 @@
+// string
+let firstName:string='ijaz'
+// number
+let roll:number=123
+// boolean
+let isAdmin:boolean=true
+// undefined
+let x:undefined=undefined
+// null
+let y:null=null
+
+// Array
+let friends:string[]=['mukul','karim']
+let eligibleRollList:number[]=[1,3]
+// tuple --> array --> order --> type of values
+let coordinates:[number,number]=[1,5]
+let ageName:[number,string,boolean] =[50,'Mr. X', true]
